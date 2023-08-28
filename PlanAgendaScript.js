ee44642fd705f4ad874b2f1d068c6fdd315442a2
Cybo3D <technoboykaj@gmail.com>
2023-08-28 12:10:30 +0200

    Revert "added contact, refreshes page when creating plan, removed unworking nav functions"
    This reverts commit 1a7b9d638225e617691db9e10f4f4d6e8bb3223e.

--- PlanAgendaScript.js
+++ PlanAgendaScript.js
@@ -188,8 +188,6 @@ function CreatePlan(id)
 
     CreateId = Name + "|" + Description + "|" + WeekDay + "|" + Year + "|" + Month + "|" + Week + "|" + Day + "|" + Priority + "|" + Top + "|" + Planned + "|" + Done + "|" + Color;
     CreatePlanExecute(CreateId);
-
-    window.location.reload();
 }
 
 
